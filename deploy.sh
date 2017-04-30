6eaecc1ecfd5643dd7ac84f5c972f57552f7c5b3
Bart Bania <contact@bartbania.com>
2017-04-30 22:24:05 +0100

    deploy script changes

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,17 @@ set -e
 
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ $# -ne 2 ]]; then
-    echo "You must set a version number and image name"
-    echo "./deploy.sh <version> <img_name>"
+if [[ $# -ne 3 ]]; then
+    echo "You must set a version number and image name as well as push flag"
+    echo "./deploy.sh <version> <img_name> <(y|n)>"
     exit 1
 fi
 
 version=$1
 name=$2
+dpush=$3
 
-dockerfile_version=$(grep VERSION= ${base}/Dockerfile | cut -d= -f2)
+dockerfile_version=$(grep -i ${name}_VERSION= ${base}/Dockerfile | cut -d= -f2)
 
 if [[ $version != $dockerfile_version ]]; then
     echo "Version mismatch in 'Dockerfile'"
@@ -25,5 +26,9 @@ fi
 echo "Building docker images for ${name}:${version}..."
 docker build -f "${base}/Dockerfile" -t bbania/${name}:${version} .
 
-echo "Uploading docker images for ${name} ${version}..."
-docker push bbania/${name}:${version}
+if [[ ${dpush} == "y" ]]; then
+    echo "Uploading docker images for ${name} ${version}..."
+    docker push bbania/${name}:${version}
+elif [[ ${dpush} == "n" ]]; then
+    echo "Will not upload docker images for ${name} ${version}"
+fi
